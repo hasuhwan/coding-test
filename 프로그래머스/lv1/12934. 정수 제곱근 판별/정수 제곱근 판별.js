466279bae0a7ab6338b1b4f97c5caff5ff097dad
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2022-11-29 17:48:43 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 30.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  var answer = 0;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
+    if (i ** 2 === n) {
+      answer = (i + 1) ** 2;
+      break;
+    } else {
+      answer = -1;
+    }
+  }
+  return answer;
+}
