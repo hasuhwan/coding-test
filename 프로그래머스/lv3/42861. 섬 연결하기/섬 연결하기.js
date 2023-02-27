d255213f8a03ecfc8493a72c6287dac55584eedb
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-02-27 22:23:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,48 +1,15 @@
 function solution(n, costs) {
-  var answer = 0;
-  costs.sort((a, b) => a[2] - b[2]);
-  let group = [];
-  let edgeCount = 0;
-  function groupCheck(start, end) {
-    const merge = [];
-    for (let i = 0; i < group.length; i++) {
-      const startCheck = group[i].includes(start);
-      const endCheck = group[i].includes(end);
-      if (startCheck && endCheck) {
-        return false;
-      } else if (startCheck && !endCheck) {
-        merge.push([...group[i], end]);
-        group.splice(i, 1);
-        i -= 1;
-      } else if (!startCheck && endCheck) {
-        merge.push([...group[i], start]);
-        group.splice(i, 1);
-        i -= 1;
-      } else {
-        continue;
-      }
+    costs.sort((a,b) => a[2] - b[2]);
+    let [from, to, answer] = costs.shift();
+    let connected = new Set([from, to]);
+    while (connected.size < n) {
+        let index = costs.findIndex(([from, to]) =>
+                                    connected.has(from) && !connected.has(to)
+                                    || connected.has(to) && !connected.has(from)
+                                   );
+        let [[from, to, cost]] = costs.splice(index, 1);
+        answer += cost;
+        connected.add(from).add(to);
     }
-    if (merge.length === 2) {
-      const set = [...new Set([...merge[0], ...merge[1]])];
-      group.push(set);
-    } else if (merge.length === 1) {
-      group.push(merge[0]);
-    } else if (merge.length === 0) {
-      group.push([start, end]);
-    }
-  }
-
-  for (let i = 0; i < costs.length; i++) {
-    const [start, end, weight] = costs[i];
-    const temp = groupCheck(start, end);
-    if (temp === false) {
-      continue;
-    } else {
-      answer += weight;
-      edgeCount += 1;
-    }
-    if (edgeCount === n - 1) {
-      return answer;
-    }
-  }
+    return answer;
 }
