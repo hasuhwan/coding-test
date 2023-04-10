03ef3cf293f382bd9c8c4144fa48142f0cc6f634
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-10 12:57:06 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,32 +1,33 @@
 function solution(id_list, report, k) {
-  const userObj = {};
-  const reportObj = {};
-  for (let i = 0; i < id_list.length; i++) {
-    userObj[id_list[i]] = 0;
-  }
-  const set = new Set(report);
-  const reportArr = [...set].map((el) => {
-    return el.split(" ");
-  });
-  reportArr.forEach((el) => {
-    if (reportObj[el[1]] === undefined) {
-      reportObj[el[1]] = 1;
-    } else {
-      reportObj[el[1]] += 1;
+    var answer = [];
+    const idObj={};
+    const reportObj={};
+    for(let i=0; i<report.length; i++){
+        const [author,people]=report[i].split(" ");
+     
+        if(idObj[author]){
+            if(idObj[author].includes(people)){
+            continue;
+        }
+            idObj[author].push(people);
+        }else{
+            idObj[author]=[people];
+        }
+        reportObj[people]=reportObj[people]?reportObj[people]+1:1;
     }
-  });
-  const reportUserArr = [];
-  for (const key in reportObj) {
-    if (reportObj[key] >= k) {
-      reportUserArr.push(key);
+    for(let i=0; i<id_list.length; i++){
+    if(idObj[id_list[i]]){
+            const temp=idObj[id_list[i]].reduce((acc,cur)=>{
+                if(reportObj[cur]>=k){
+                    return acc+1;
+                }else{
+                    return acc;
+                }
+            },0)
+            answer.push(temp);
+        }else{
+            answer.push(0);
+        }
     }
-  }
-  reportUserArr.forEach((el) => {
-    reportArr.forEach((user) => {
-      if (user[1] === el) {
-        userObj[user[0]] += 1;
-      }
-    });
-  });
-  return Object.values(userObj);
+    return answer;
 }
