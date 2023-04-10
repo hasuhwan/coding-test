c06e5894d640697d02b38abb260de1a8e6af4c55
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-10 13:26:55 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.34 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,64 +1,33 @@
 function solution(fees, records) {
-  var answer = [];
-  const obj = {};
-  for (let i = 0; i < records.length; i++) {
-    const newArr = records[i].split(" ");
-    const splitTime = newArr[0].split(":").map((str) => Number(str));
-    if (obj[newArr[1]] === undefined) {
-      obj[newArr[1]] = [];
-      if (newArr[2] === "IN") {
-        obj[newArr[1]][0] = -(splitTime[0] * 60 + splitTime[1]);
-        obj[newArr[1]][1] = "IN";
-      }
-    } else {
-      if (newArr[2] === "IN") {
-        obj[newArr[1]][1] = "IN";
-        obj[newArr[1]][0] -= splitTime[0] * 60 + splitTime[1];
-      } else {
-        obj[newArr[1]][1] = "OUT";
-        obj[newArr[1]][0] += splitTime[0] * 60 + splitTime[1];
-      }
+    var answer = [];
+    const [basicM,basicC,perM,perC]=fees;
+    const obj={};
+    records=records.map(el=>{
+        const [timeH,num,type]=el.split(" ");
+        const timeM=timeH.split(":").reduce((acc,cur,idx)=>{
+           if(idx===0){
+               return Number(cur)*60;
+           } else{
+               return acc+Number(cur);
+           }
+        },0);
+        if(!obj[num]){
+            obj[num]={IN:[],OUT:[]};
+        }
+        obj[num][type].push(timeM);
+    })
+for(let key in obj){
+    if(obj[key].IN.length!==obj[key].OUT.length){
+        obj[key].OUT.push(24*60-1);
     }
-  }
-  console.log(obj);
-  const keys = Object.keys(obj).sort((a, b) => Number(a) - Number(b));
-  console.log(keys);
-  for (let i = 0; i < keys.length; i++) {
-    if (obj[keys[i]][1] === "IN") {
-      obj[keys[i]][0] += 23 * 60 + 59;
-      if (fees[0] >= obj[keys[i]][0]) {
-        answer.push(fees[1]);
-      } else {
-        answer.push(
-          fees[1] + Math.ceil((obj[keys[i]][0] - fees[0]) / fees[2]) * fees[3]
-        );
-      }
-    } else {
-      if (fees[0] >= obj[keys[i]][0]) {
-        answer.push(fees[1]);
-      } else {
-        answer.push(
-          fees[1] + Math.ceil((obj[keys[i]][0] - fees[0]) / fees[2]) * fees[3]
-        );
-      }
+     const totalM=obj[key].OUT.reduce((acc,cur)=>acc+cur,0)-obj[key].IN.reduce((acc,cur)=>acc+cur,0);
+    if(totalM<=basicM){
+        answer.push([basicC,Number(key)]);
+    }else{
+    const totalC=basicC+Math.ceil((totalM-basicM)/perM)*perC;
+        answer.push([totalC,Number(key)]);
     }
-  }
-
-  console.log(answer);
-  return answer;
+    
 }
-
-solution(
-  [180, 5000, 10, 600],
-  [
-    "05:34 5961 IN",
-    "06:00 0000 IN",
-    "06:34 0000 OUT",
-    "07:59 5961 OUT",
-    "07:59 0148 IN",
-    "18:59 0000 IN",
-    "19:09 0148 OUT",
-    "22:59 5961 IN",
-    "23:00 5961 OUT",
-  ]
-);
+    return answer.sort((a,b)=>a[1]-b[1]).map(el=>el[0]);
+}
