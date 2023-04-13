b1a3585030978456358529f9d38fb5081dd8b536
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-13 15:13:46 +0900

    [level 2] Title: 방문 길이, Time: 0.84 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,23 +1,30 @@
 function solution(dirs) {
-  let x = 0;
-  let y = 0;
-  const preRoad = [];
-  const directionObj = {
-    R: [1, 0],
-    L: [-1, 0],
-    U: [0, 1],
-    D: [0, -1],
-  };
-  for (let i = 0; i < dirs.length; i++) {
-    const nextX = x + directionObj[dirs[i]][0];
-    const nextY = y + directionObj[dirs[i]][1];
-    if (Math.abs(nextX) <= 5 && Math.abs(nextY) <= 5) {
-      const road = `${nextX + x}, ${nextY + y}`;
-      preRoad.push(road);
-      x = nextX;
-      y = nextY;
+    var answer = 0;
+    const visited=[];
+    const dir={
+        U:[0,1],
+        D:[0,-1],
+        R:[1,0],
+        L:[-1,0]
+    };
+    let start=[0,0];
+    for(let i=0; i<dirs.length; i++){
+        const [stepX,stepY]=dir[dirs[i]];
+        const [X,Y]=start;
+        const [nextX,nextY]=[stepX+X,stepY+Y];
+        if(nextX<-5||nextY<-5||nextX>5||nextY>5){
+            continue;
+        }
+        const tempVisit=`${(nextX+X)/2},${(nextY+Y)/2}`
+        if(!visited.includes(tempVisit)){
+            visited.push(tempVisit);
+            answer++;
+        }
+        start=[nextX,nextY];
+
+        
+      
+        
     }
-  }
-  const set = new Set(preRoad);
-  return set.size;
+    return answer;
 }
