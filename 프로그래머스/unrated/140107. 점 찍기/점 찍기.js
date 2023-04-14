44ded5f36f88f84b6113f389f46199d95b4d0ef2
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-14 11:07:54 +0900

    [unrated] Title: 점 찍기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.js
@@ -1,14 +1,9 @@
 function solution(k, d) {
-  var answer = 0;
-  const x = d / k;
-  for (let i = 0; i <= Math.floor(x); i++) {
-    const temp = Math.pow(x, 2) - Math.pow(i, 2);
-    const b = Math.floor(Math.sqrt(temp));
-    if (b === 0) {
-      answer++;
-    } else {
-      answer += b + 1;
+    var answer = 0;
+    for(let i=0; i<=d; i+=k){
+        const temp=Math.floor(Math.sqrt(Math.pow(d,2)-Math.pow(i,2)));
+        answer+=Math.floor(temp/k+1);
+        
     }
-  }
-  return answer;
+    return answer;
 }
