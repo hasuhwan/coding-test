8f378e0b0496269d0daec7e99dfaf48f5b4a8a8e
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-19 13:30:06 +0900

    [level 2] Title: 오픈채팅방, Time: 129.14 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,24 +1,20 @@
 function solution(record) {
-  var answer = [];
-  const obj = {};
-  const splitArr = [];
-  for (let i = 0; i < record.length; i++) {
-    splitArr.push(record[i].split(" "));
-    if (splitArr[i][0] === "Enter" || splitArr[i][0] === "Change") {
-      obj[splitArr[i][1]] = splitArr[i][2];
+    var answer = [];
+    const obj={};
+    record=record.map(el=>el.split(" "));
+    record.forEach(el=>{
+if(el[0]==="Enter"||el[0]==="Change"){
+        obj[el[1]]=el[2];
+}
+    
+    })
+record.forEach(el=>{
+    const [command,user]=el;
+    if(command==="Enter"){
+        answer.push(`${obj[user]}님이 들어왔습니다.`);
+    }else if(command==="Leave"){
+        answer.push(`${obj[user]}님이 나갔습니다.`);
     }
-  }
-
-  for (let i = 0; i < splitArr.length; i++) {
-    if (splitArr[i][0] === "Enter") {
-      const text = `${obj[splitArr[i][1]]}님이 들어왔습니다.`;
-      answer.push(text);
-    } else if (splitArr[i][0] === "Leave") {
-      const text = `${obj[splitArr[i][1]]}님이 나갔습니다.`;
-      answer.push(text);
-    } else {
-      continue;
-    }
-  }
-  return answer;
+})
+    return answer;
 }
