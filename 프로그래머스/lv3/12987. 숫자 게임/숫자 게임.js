fe4db043c68b55283d7fee76b5ab1627e9c3ba8c
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-04-19 13:54:12 +0900

    [level 3] Title: 숫자 게임, Time: 87.37 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.js
@@ -1,17 +1,21 @@
 function solution(A, B) {
-  var answer = 0;
-  let sortedA = A.sort((a, b) => a - b);
-  let sortedB = B.sort((a, b) => a - b);
-  let i = 0;
-  let j = 0;
-  while (sortedB.length !== i) {
-    if (sortedB[i] > sortedA[j]) {
-      j++;
-      i++;
-      answer++;
-    } else {
-      i++;
+    var answer = 0;
+    A.sort((a,b)=>b-a);
+    B.sort((a,b)=>b-a);
+    let a=0;
+    let b=0;
+    
+    while(a!==A.length){
+        if(A[a]===B[b]){
+            a++;
+            
+        }else if(A[a]>B[b]){
+            a++;
+        }else{
+            answer++;
+            a++;
+            b++;
+        }
     }
-  }
-  return answer;
+    return answer;
 }
