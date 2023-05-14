9c507f97ba057622ed333fd65f376d1721270ed3
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-05-14 11:14:03 +0900

    [level 3] Title: 섬 연결하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,15 +1,32 @@
 function solution(n, costs) {
-    costs.sort((a,b) => a[2] - b[2]);
-    let [from, to, answer] = costs.shift();
-    let connected = new Set([from, to]);
-    while (connected.size < n) {
-        let index = costs.findIndex(([from, to]) =>
-                                    connected.has(from) && !connected.has(to)
-                                    || connected.has(to) && !connected.has(from)
-                                   );
-        let [[from, to, cost]] = costs.splice(index, 1);
-        answer += cost;
-        connected.add(from).add(to);
+  var answer = 0;
+  costs.sort((a, b) => a[2] - b[2]);
+  const parentArr = new Array(n).fill(0).map((_, idx) => idx);
+  function getParent(node) {
+    if (parentArr[node] === node) {
+      return node;
+    } else {
+      return getParent(parentArr[node]);
     }
-    return answer;
+  }
+  function unionFind(a, b) {
+    const one = getParent(a);
+    const two = getParent(b);
+    if (one === two) {
+      return false;
+    } else {
+      if (one > two) {
+        parentArr[one] = two;
+      } else {
+        parentArr[two] = one;
+      }
+      return true;
+    }
+  }
+  for (let i = 0; i < costs.length; i++) {
+    if (unionFind(costs[i][0], costs[i][1])) {
+      answer += costs[i][2];
+    }
+  }
+  return answer;
 }
