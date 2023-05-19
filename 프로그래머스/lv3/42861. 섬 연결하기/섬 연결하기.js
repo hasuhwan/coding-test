28081c60a972b22ef18b6f9a123ee4d7b4945d8c
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-05-19 13:56:45 +0900

    [level 3] Title: 섬 연결하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -1,79 +1,34 @@
-class Heap {
-  constructor() {
-    this.heap = [];
-  }
-
-  insert(arr) {
-    const heap = this.heap;
-    arr.forEach((el) => {
-      heap.push(el);
-      let index = heap.length - 1;
-      let parent = Math.floor((index - 1) / 2);
-      while (index > 0) {
-        if (heap[index].cost < heap[parent].cost) {
-          [heap[index], heap[parent]] = [heap[parent], heap[index]];
-        }
-        index = parent;
-        parent = Math.floor((index - 1) / 2);
-      }
-    });
-  }
-
-  pop() {
-    const heap = this.heap;
-    const result = heap[0];
-    if (heap.length === 0) {
-      return;
-    }
-    if (heap.length === 1) {
-      heap.pop();
-      return result;
-    }
-    heap[0] = heap.pop();
-
-    let index = 0;
-    while (index < heap.length) {
-      let left = index * 2 + 1;
-      let right = index * 2 + 2;
-      let next = index;
-
-      if (heap[next].cost > heap[right]?.cost) {
-        next = right;
-      }
-      if (heap[next].cost > heap[left]?.cost) {
-        next = left;
-      }
-      if (next === index) {
-        break;
-      }
-      [heap[index], heap[next]] = [heap[next], heap[index]];
-      index = next;
-    }
-    return result;
-  }
-}
-
 function solution(n, costs) {
   var answer = 0;
-  const visited = new Array(n).fill(false);
-  const arr = new Array(n).fill(0).map(() => []);
-  costs.forEach(([from, to, cost]) => {
-    arr[from].push({ to, cost });
-    arr[to].push({ to: from, cost });
-  });
-
-  const heap = new Heap();
-  heap.insert(arr[0]);
-
-  visited[0] = true;
-  while (heap.heap.length !== 0) {
-    const temp = heap.pop();
-    if (visited[temp.to] === true) {
-      continue;
+  costs.sort((a, b) => a[2] - b[2]);
+  const parentArr = new Array(n).fill(0).map((_, idx) => idx);
+  function getParent(node) {
+    if (parentArr[node] === node) {
+      return node;
+    } else {
+        const currentParent=getParent(parentArr[node]);
+        parentArr[node]=currentParent;
+      return currentParent;
     }
-    heap.insert(arr[temp.to]);
-    visited[temp.to] = true;
-    answer += temp.cost;
+  }
+  function unionFind(a, b) {
+    const one = getParent(a);
+    const two = getParent(b);
+    if (one === two) {
+      return false;
+    } else {
+      if (one > two) {
+        parentArr[one] = two;
+      } else {
+        parentArr[two] = one;
+      }
+      return true;
+    }
+  }
+  for (let i = 0; i < costs.length; i++) {
+    if (unionFind(costs[i][0], costs[i][1])) {
+      answer += costs[i][2];
+    }
   }
   return answer;
 }
