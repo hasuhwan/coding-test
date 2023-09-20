de52e9ed8725fc6822ff21ef536e33a04bb84b8a
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-20 13:37:05 +0900

    [level 3] Title: 아이템 줍기, Time: 2.91 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.js
+++ 프로그래머스/lv3/87694. 아이템 줍기/아이템 줍기.js
@@ -1,43 +1,38 @@
 function solution(rectangle, characterX, characterY, itemX, itemY) {
-    var answer = 0;
-    characterX=characterX*2;
-    characterY=characterY*2;
-    itemX=itemX*2;
-    itemY=itemY*2;
-    rectangle=rectangle.map(el=>el.map(v=>v*2));
-    const scale=new Array(103).fill(0).map(()=>new Array(103).fill(0));
-rectangle.forEach(el=>{
-    const [lbx,lby,rtx,rty]=el;
-    for(let i=lbx; i<=rtx; i++){
-        for(let j=lby; j<=rty; j++){
-            if(i===lbx||i===rtx||j===lby||j===rty){
-                if(scale[i][j]===0){
-                    scale[i][j]=1;
+    rectangle=rectangle.map(arr=>arr.map(el=>el*2));
+    [characterX, characterY, itemX, itemY]=[characterX, characterY, itemX, itemY].map(el=>el*2);
+    console.log(characterX, characterY, itemX, itemY);
+    const arr=new Array(104).fill(0).map(()=>new Array(104).fill(0));
+    rectangle.forEach(([x1,y1,x2,y2])=>{
+        for(let i=y1; i<=y2; i++){
+            for(let j=x1; j<=x2; j++){
+                if(i===y1||i===y2||j===x1||j===x2){
+                    if(arr[j][i]===1){
+                        continue;
+                    }
+                    arr[j][i]+=1;
+                }else{
+                    arr[j][i]+=2;
                 }
-            }else{
-                scale[i][j]=2;
             }
         }
-    }
-})
-    const direction=[[1,0],[-1,0],[0,1],[0,-1]];
-    const queue=[[characterX, characterY,0]];
-    scale[characterX][characterY]=0;
+    });
+    const dir=[[1,0],[-1,0],[0,1],[0,-1]];
+    const queue=[[characterX,characterY,0]];
+    arr[characterX][characterY]=2;
     while(queue.length!==0){
-        const [X,Y,count]=queue.shift();
-        if(X===itemX&&Y===itemY){
+        const [x,y,count]=queue.shift();
+        if(x===itemX&&y===itemY){
             return count/2;
         }
-        for(let i=0; i<direction.length; i++){
-            const [stepX,stepY]=direction[i];
-            const [nextX,nextY]=[stepX+X,stepY+Y];
-    if(scale[nextX][nextY]===1){
-        queue.push([nextX,nextY,count+1]);
-        scale[nextX][nextY]=0;
+        dir.forEach(([dirx,diry])=>{
+            const [nextx,nexty]=[x+dirx,y+diry];
+            if(nextx<0||nextx>=104||nexty<0||nexty>=104||arr[nextx][nexty]!==1){
+                return;
+            }
+            arr[nextx][nexty]=2;
+            queue.push([nextx,nexty,count+1]);
+            
+        })
     }
-        }
-    }
-    
-    
-    return answer;
 }
