12094d405b1bb79ffb07de589cc8f72d559bd928
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-20 09:43:17 +0900

    [level 2] Title: 타겟 넘버, Time: 1.21 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,14 +1,14 @@
 function solution(numbers, target) {
     var answer = 0;
-    function dfs(count,sum){
-        if(count===numbers.length){
-            if(sum===target){
+    function dfs(idx,temp){
+        if(idx===numbers.length){
+            if(temp===target){
                 answer++;
             }
             return;
         }
-        dfs(count+1,sum+numbers[count]);
-        dfs(count+1,sum-numbers[count]);
+        dfs(idx+1,temp+numbers[idx]);
+        dfs(idx+1,temp-numbers[idx]);
     }
     dfs(0,0);
     return answer;
