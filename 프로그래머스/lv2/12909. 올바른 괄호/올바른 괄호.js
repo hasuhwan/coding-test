585f245db731fe04e91b67ff1c7dfff5238c2cd1
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-21 11:48:51 +0900

    [level 2] Title: 올바른 괄호, Time: 5.42 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,17 +1,18 @@
-//l l r r
-function solution(s) {
-  let stack = [];
-  let count = 0;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] === "(") {
-      stack.push(s[i]);
-      count++;
-    } else if (s[i] === ")" && stack.length === 0) {
-      return false;
-    } else {
-      stack.pop();
-      count--;
+function solution(s){
+    var answer = true;
+    const stack=[];
+    for(let i=0; i<s.length; i++){
+        if(s[i]===")"){
+            if(stack[stack.length-1]==="("){
+            stack.pop();    
+            }else{
+                return false;
+            }
+            
+        }else{
+            stack.push(s[i]);
+        }
     }
-  }
-  return count === 0 ? true : false;
+
+    return stack.length===0;
 }
