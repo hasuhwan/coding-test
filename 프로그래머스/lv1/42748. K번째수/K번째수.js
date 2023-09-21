792cf1d89af914e976aefa694a43d4ecda5aa898
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-21 09:34:17 +0900

    [level 1] Title: K번째수, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,18 +1,7 @@
 function solution(array, commands) {
-  const answer = [];
-  let i = 0;
-  let j = 0;
-  let k = 0;
-
-  for (m = 0; m < commands.length; m++) {
-    i = commands[m][0];
-    j = commands[m][1];
-    k = commands[m][2];
-
-    let sliced = array.slice(i - 1, j);
-    let sorted = sliced.sort((a, b) => a - b);
-    answer.push(sorted[k - 1]);
-  }
-
-  return answer;
+    var answer = [];
+    commands.forEach(([i,j,k])=>{
+        answer.push(array.slice(i-1,j).sort((a,b)=>a-b)[k-1]);
+    })
+    return answer;
 }
