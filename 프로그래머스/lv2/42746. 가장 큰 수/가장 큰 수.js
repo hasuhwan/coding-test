6ba91fb464e8d2ab5e12366d66cd708aa9ccfe7f
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-21 09:48:28 +0900

    [level 2] Title: 가장 큰 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,9 +1,4 @@
 function solution(numbers) {
-  var answer = "";
-  numbers = numbers.map((a) => a + "").sort((a, b) => b + a - (a + b));
-  answer = numbers.join("");
-  if (answer[0] === "0") {
-    answer = "0";
-  }
-  return answer;
+    const answer=numbers.map((num)=>String(num)).sort((a,b)=>Number(b+a)-Number(a+b)).join("");
+    return answer[0]==="0"?"0":answer;
 }
