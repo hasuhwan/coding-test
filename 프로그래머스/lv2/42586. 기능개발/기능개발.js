de5e7d4be702629e55c4b556e408bfcfb0498a83
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-09-21 12:00:18 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,20 +1,20 @@
 function solution(progresses, speeds) {
-  var answer = [];
-  const arr = [];
-  let count = 1;
-  for (let i = 0; i < progresses.length; i++) {
-    arr[i] = Math.ceil((100 - progresses[i]) / speeds[i]);
-  }
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (max >= arr[i]) {
-      count++;
-    } else {
-      max = arr[i];
-      answer.push(count);
-      count = 1;
-    }
-  }
-  answer.push(count);
-  return answer;
+    var answer = [];
+    let max=0;
+    let count=0;
+    progresses.forEach((el,idx)=>{
+        const days=Math.ceil((100-el)/speeds[idx]);
+        if(max===0){
+            count=1;
+            max=days;
+        }else if(max>=days){
+            count++;
+        }else{
+            answer.push(count);
+            max=days;
+            count=1;
+        }
+    })
+    answer.push(count);
+    return answer;
 }
