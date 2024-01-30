f1cb2c7170ac0796081a7ec574d68270a7da2c42
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-01-30 14:19:47 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+    if(a%2!==0&&b%2!==0){
+        return Math.pow(a,2)+Math.pow(b,2);
+    }else if(a%2===0&&b%2===0){
+        return Math.abs(a-b);
+    }
+    return 2*(a+b);
+}
