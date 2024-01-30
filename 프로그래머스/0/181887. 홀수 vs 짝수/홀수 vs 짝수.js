5811358a3f66e54c4969840242947473a828a8c2
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-01-30 13:56:19 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    let odd=0;
+    let even=0;
+    num_list.forEach((num,idx)=>{
+        if(idx%2===0){
+            odd+=num;
+        }else{
+            even+=num;
+        }
+    });
+    return odd>even?odd:even;
+}
