ce5f55b00ae3a3819341ed5cfcfe888bc3aec60a
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-02-27 09:37:09 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -1,13 +1,9 @@
 function solution(bandage, health, attacks) {
     let tempHealth=health;
-    let prev=attacks[0][0];
-    tempHealth-=attacks[0][1];
-    if(tempHealth<=0){
-        return -1;
-    }
+    let prev=0;
+   
     const [doing,add,ex]=bandage;
-    for(let i=1; i<attacks.length; i++){
-        const [time,damage]=attacks[i];
+    for(let [time,damage] of attacks ){
         const diff=time-prev-1;
         const extraAdd=Math.floor(diff/doing);
         tempHealth=Math.min(tempHealth+(add*diff)+(extraAdd*ex),health)-damage;
