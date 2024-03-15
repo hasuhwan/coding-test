5740621751178f09f8c3ce05ec1b76b238d926ee
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-03-15 12:29:14 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 74.29 ms, Memory: 57.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,13 +1,18 @@
 function solution(elements) {
     const set=new Set();
-    const arr=elements.concat(elements);
-    for(let i=0; i<elements.length; i++){
+    const len=elements.length;
+    for(let i=1; i<=len; i++){
         let sum=0;
-        for(let j=0; j<elements.length; j++){
-            sum+=arr[i+j];
+        for(let j=0; j<len; j++){
+            if(j===0){
+                for(let k=0; k<i; k++){
+                    sum+=elements[k];
+                }
+            }else{
+                sum=sum-elements[j-1]+elements[(j+i-1)%len];
+            }
             set.add(sum);
         }
     }
-    
     return set.size;
 }
