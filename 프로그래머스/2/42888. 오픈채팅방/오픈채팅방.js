655309f0372260291826bd3ba90e3143f329808a
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-03-19 13:01:22 +0900

    [level 2] Title: 오픈채팅방, Time: 140.67 ms, Memory: 98.8 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,18 +1,20 @@
 function solution(record) {
     var answer = [];
+    const order=[];
     const map=new Map();
-    record.forEach(str=>{
-        const [doing,id,nick]=str.split(" ");
-        if(doing==="Enter"||doing==="Change"){
-            map.delete(id);
+    record.forEach((str)=>{
+        const [o,id,nick]=str.split(" ");
+        if(o==="Enter"||o==="Leave"){
+            order.push([o,id]);
+        }
+        if(o!=="Leave"){
             map.set(id,nick);
         }
     });
-    record.forEach(str=>{
-        const [doing,id,nick]=str.split(" ");
-        if(doing==="Enter"){
+    order.forEach(([o,id])=>{
+        if(o==="Enter"){
             answer.push(`${map.get(id)}님이 들어왔습니다.`);
-        }else if(doing==="Leave"){
+        }else{
             answer.push(`${map.get(id)}님이 나갔습니다.`);
         }
     })
