1ddd43fb14bc949851e1f1c8aacd930c15cdfcbd
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-03-21 09:56:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 14.85 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,31 +1,25 @@
 function solution(queue1, queue2) {
     var answer = 0;
-    const max=queue1.length*4;
-    function sum(arr){
-        return arr.reduce((acc,cur)=>acc+cur,0);
-    }
-    let sum1=sum(queue1);
-    let sum2=sum(queue2);
+    let sum1=queue1.reduce((acc,cur)=>acc+cur,0);
+    let sum2=queue2.reduce((acc,cur)=>acc+cur,0);
     let idx1=0;
     let idx2=0;
-    while(answer<max){
+    const totaln=queue1.length+queue2.length;
+    
+    while(sum1!==sum2){
         if(sum1>sum2){
-            const curr=queue1[idx1];
-            sum1-=curr;
-            sum2+=curr;
-            queue2.push(curr);
-            idx1++;
-        }else if(sum1<sum2){
-            const curr=queue2[idx2];
-            sum1+=curr;
-            sum2-=curr;
-            queue1.push(curr);
-            idx2++;
+            sum1-=queue1[idx1];
+            queue2.push(queue1[idx1]);
+            sum2+=queue1[idx1++];
         }else{
-            return answer;
+            sum2-=queue2[idx2];
+            queue1.push(queue2[idx2]);
+            sum1+=queue2[idx2++];
         }
         answer++;
-        
+        if(idx1>totaln||idx2>totaln){
+            return -1;
+        }
     }
-    return -1;
+    return answer;
 }
