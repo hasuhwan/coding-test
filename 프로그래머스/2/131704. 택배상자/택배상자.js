190b13d3e0df5e8522bd64273631a3cc157b9115
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2024-03-21 09:30:12 +0900

    [level 2] Title: 택배상자, Time: 27.02 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -0,0 +1,12 @@
+function solution(order) {
+    var answer = 0;
+    const stack=[];
+    for(let i=1; i<=order.length; i++){
+        stack.push(i);
+        while(stack.length!==0&&stack[stack.length-1]===order[answer]){
+            stack.pop();
+            answer++;
+        }
+    }
+    return answer;
+}
